ed80d8724586e36c1773d119ea8627472f92533e
moms-spaghetti <williamedwards36@aol.com>
2021-01-22 22:20:36 +0000

    adjusted port for api request 80 > 443

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import './App.css';
-import InputCity from '../src/components/inputCity'
-import WeatherData from '../src/components/displayWeatherData'
-require('dotenv').config()
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import InputCity from "../src/components/inputCity";
+import WeatherData from "../src/components/displayWeatherData";
+require("dotenv").config();
 
 function App() {
-  const [weatherData, setWeatherData] = useState({ name: null })
-  const [location, setLocation] = useState(null)
+  const [weatherData, setWeatherData] = useState({ name: null });
+  const [location, setLocation] = useState(null);
 
   useEffect(() => {
     if (location !== null) {
-      const apiKey = process.env.REACT_APP_API
-      const units = `metric`
+      const apiKey = process.env.REACT_APP_API;
+      const units = `metric`;
       async function getWeather() {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location},GB&units=${units}&appid=${apiKey}`)
-        const data = await response.json()
-        setWeatherData(data)
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${location},GB&units=${units}&appid=${apiKey}`
+        );
+        const data = await response.json();
+        setWeatherData(data);
       }
-      getWeather()
+      getWeather();
     }
-  }, [location])
+  }, [location]);
 
-  console.log(weatherData)
+  console.log(weatherData);
 
   return (
     <div className="App">
-    <h1>UK Weather</h1>
+      <h1>UK Weather</h1>
       <div>
         <InputCity setLocation={setLocation} />
         <WeatherData data={weatherData} />
